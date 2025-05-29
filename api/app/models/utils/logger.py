a773af649e4e78d091858ef83fb3d166b0a213ae
daria425 <darianaumova5@gmail.com>
2025-05-29 22:05:39 +0300

    add logger

---
+++ api/app/models/utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+class Logger:
+    @staticmethod
+    def configure_logging():
+        logging.basicConfig(
+            level=logging.INFO,  # Default log level
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.StreamHandler()  # Default handler to stream logs to console
+            ]
+        )
+
+    @staticmethod
+    def get_logger():
+        # Return the root logger when no name is provided
+        return logging.getLogger()
